9d5518758a8dfab540addf3cdfe020fc1b0272be
j842 <j@842.be>
2016-02-27 18:43:51 +1300

    Remove unnecessary files

--- buildnum/build_number.h
+++
@@ -1,9 +0,0 @@
-#ifndef BUILD_NUMBER_STR
-#define BUILD_NUMBER_STR "0.1 r.329"
-#endif
-#ifndef VERSION_STR
-#define VERSION_STR "0.1 r.329 - Sat Feb 27 18:36:52 NZDT 2016"
-#endif
-#ifndef VERSION_STR_SHORT
-#define VERSION_STR_SHORT "0.1 r.329"
-#endif
