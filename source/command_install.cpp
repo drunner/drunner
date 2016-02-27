68cc020047b3e241be9e0102d2c1c887097e077d
j842 <j@842.be>
2016-02-27 21:50:11 +1300

    Refactor to split out variables.sh generation (so we can read it in again later)

--- source/command_install.cpp
+++ source/command_install.cpp
@@ -23,36 +23,63 @@ using namespace utils;
       return s;
    }
 
-   class servicecfg : public settingsbash
+
+   class sh_servicecfg : public settingsbash
    {
    public:
-      std::vector<std::string> volumes,extracontainers,dockervols,dockeropts;
-
-      servicecfg(
+      // read ctor
+      sh_servicecfg(
          const params & p,
          const drunner_settings & settings,
-         std::string pwd,
+         std::string path
+         )
+         :  settingsbash(p,path+"/servicecfg.sh")
+      {
+         std::vector<std::string> nothing;
+         setSettingv("VOLUMES",nothing);
+         setSettingv("EXTRACONTAINERS",nothing);
+
+         bool readok = readSettings();
+         if (!readok)
+            logmsg(kLERROR,"Not a valid dService. Couldn't read "+path+"/servicecfg.sh",p);
+      } // ctor
+
+      void getVolumes(std::vector<std::string> & volumes) const
+         {getSettingv("VOLUMES",volumes);}
+      void getExtraContainers(std::vector<std::string> & extracontainers) const
+         {getSettingv("EXTRACONTAINERS",extracontainers);}
+   }; //class
+
+
+   class sh_variables : public settingsbash
+   {
+   public:
+
+      // reading ctor
+      sh_variables(const params & p, std::string path)
+         : settingsbash(p,path+"/variables.sh")
+         {
+         bool readok = readSettings();
+         if (!readok)
+            logmsg(kLERROR,"Broken dService. Couldn't read "+path+"/variables.sh",p);
+         }
+
+      // writing ctor
+      sh_variables(
+         const params & p,
+         const drunner_settings & settings,
+         std::string path,
+         const sh_servicecfg & servicecfg,
          std::string servicename,
          std::string imagename,
          std::string hostIP,
          std::string serviceTempDir
          )
-         :  settingsbash(p,pwd+"/servicecfg.sh")
+         :  settingsbash(p,path+"/variables.sh")
       {
-         std::vector<std::string> nothing;
-         setSettingv("VOLUMES",nothing);
-         setSettingv("EXTRACONTAINERS",nothing);
-         setSetting("SERVICENAME",servicename);
-         setSetting("IMAGENAME",imagename);
-         setSetting("INSTALLTIME",utils::getTime());
-         setSetting("HOSTIP",hostIP);
-         setSetting("SERVICETEMPDIR",serviceTempDir);
-
-         bool readok = readSettings();
-         if (!readok)
-            logmsg(kLERROR,"Not a valid dService. Couldn't read servicecfg.sh file from "+pwd,p);
-         getSettingv("VOLUMES",volumes);
-         getSettingv("EXTRACONTAINERS",extracontainers);
+         std::vector<std::string> volumes, extracontainers,dockervols,dockeropts;
+         servicecfg.getVolumes(volumes);
+         servicecfg.getExtraContainers(extracontainers);
 
          for (uint i=0;i<volumes.size();++i)
             {
@@ -62,13 +89,21 @@ using namespace utils;
             dockeropts.push_back("-v");
             dockeropts.push_back(dockervols[i]+":"+volumes[i]);
             }
-         setSettingv("DOCKERVOLS",dockervols);
-         setSettingv("DOCKEROPTS",dockeropts);
-
          for (uint i=0;i<extracontainers.size();++i)
             logmsg(kLDEBUG, "EXTRACONTAINER:  "+extracontainers[i],p);
-      } // ctor
-   }; //class
+
+         setSettingv("VOLUMES",volumes);
+         setSettingv("EXTRACONTAINERS",extracontainers);
+         setSetting("SERVICENAME",servicename);
+         setSetting("IMAGENAME",imagename);
+         setSetting("INSTALLTIME",utils::getTime());
+         setSetting("HOSTIP",hostIP);
+         setSetting("SERVICETEMPDIR",serviceTempDir);
+         setSettingv("DOCKERVOLS",dockervols);
+         setSettingv("DOCKEROPTS",dockeropts);
+         writeSettings();
+      }
+   }; // sh_variables
 
 
 
@@ -138,17 +173,20 @@ using namespace utils;
             logmsg(kLERROR,"Couldn't copy the service files. You will need to reinstall the service.",p);
 
          // read in servicecfg.sh
-         servicecfg sc(p,settings,drd,servicename,imagename,hostIP,serviceTempDir);
+         sh_servicecfg servicecfg(p,settings,drd);
+
+         // write out variables.sh
+         sh_variables variables(p,settings,drd,servicecfg,servicename,imagename,hostIP,serviceTempDir);
 
          // make sure we have the latest of all exra containers.
-         for (uint i=0;i<sc.extracontainers.size();++i)
-            command_setup::pullImage(p,settings,sc.extracontainers[i]);
+         std::vector<std::string> extracontainers;
+         servicecfg.getExtraContainers(extracontainers);
+         for (uint i=0;i<extracontainers.size();++i)
+            command_setup::pullImage(p,settings,extracontainers[i]);
 
          // create the utils.sh file for the dService.
          createutils_sh(drd,p);
 
-         // create variables.sh for the dService.
-         sc.writeSettings(drd+"/variables.sh");
       }
 
       catch (const eExit & e) {
