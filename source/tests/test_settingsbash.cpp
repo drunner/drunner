b3ddfe8958042f9637f089b6826118716fde5bd2
j842 <j@842.be>
2016-03-08 22:35:56 +1300

    Add unit test that shows failure seen in real build.

--- source/tests/test_settingsbash.cpp
+++ source/tests/test_settingsbash.cpp
@@ -9,17 +9,30 @@ TEST_CASE("read/write bool works","[settingsbash]") {
    SECTION("Test true functions")
    {
       sbelement e("bool",true);
+      REQUIRE(e.getBashLine()=="bool=yes");
+
       sb.setSetting(e);
-      REQUIRE(sb.getBool("bool")==true);
-      REQUIRE(utils::stringisame(e.getBashLine(),"bool=yes"));
+      REQUIRE(sb.getBool("bool"));
    }
 
    SECTION("Test false functions")
    {
       sbelement e("bool",false);
+      REQUIRE(e.getBashLine() == "bool=no");
+
       sb.setSetting(e);
-      REQUIRE(sb.getBool("bool")==false);
-      REQUIRE(utils::stringisame(e.getBashLine(),"bool=no"));
+      REQUIRE_FALSE(sb.getBool("bool"));
+   }
+
+   SECTION("Test bunch'o'Stuff")
+   {
+      sb.setSetting(sbelement("ROOTPATH","/home/j"));
+      sb.setSetting(sbelement("ROOTUTILIMAGE","drunner/install-rootutils"));
+      sb.setSetting(sbelement("DRUNNERINSTALLURL",R"EOF(https://drunner.s3.amazonaws.com/drunner-install)EOF"));
+      sb.setSetting(sbelement("DRUNNERINSTALLTIME",utils::getTime()));
+      sb.setSetting(sbelement("PULLIMAGES","yes"));
+
+      REQUIRE( sb.getString("ROOTUTILIMAGE")=="drunner/install-rootutils" );
    }
 
 }
