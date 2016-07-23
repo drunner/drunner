d30da6ae22e0eed1c7b76307dc498b3b78e2aa06
j842 <j@842.be>
2016-07-23 13:17:09 +1200

    Adding lua, adding cmake

---
+++ source/cmake/CMakeFiles/drunner.dir/DependInfo.cmake
@@ -0,0 +1,23 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/cygdrive/z/dev/drunner/source/cmake/CMakeFiles/3.3.2/CompilerIdCXX/CMakeCXXCompilerId.cpp" "/cygdrive/z/dev/drunner/source/cmake/CMakeFiles/drunner.dir/CMakeFiles/3.3.2/CompilerIdCXX/CMakeCXXCompilerId.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "source"
+  "buildnum"
+  "source/tests"
+  "deps"
+  "source/plugins"
+  "deps/yaml-cpp/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
---
+++ source/cmake/CMakeFiles/drunner.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/drunner.dir/CMakeFiles/3.3.2/CompilerIdCXX/CMakeCXXCompilerId.cpp.o"
+  "drunner.pdb"
+  "drunner.exe"
+  "libdrunner.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/drunner.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
