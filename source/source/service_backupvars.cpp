b42dc205633d109164b21627aa6e63da0d2aee86
j842 <j@842.be>
2016-07-30 17:27:24 +1200

    Fix backupvars on restore

--- source/source/service_backupvars.cpp
+++ source/source/service_backupvars.cpp
@@ -2,34 +2,41 @@
 #include <fstream>
 
 #include "service_backupvars.h"
+#include "dassert.h"
 
 const std::string backupvars::filename("backupvars.json");
 
 backupvars::backupvars(Poco::Path path) : mPath(path)
 {
-   poco_assert(path.isFile());
+   drunner_assert(path.isFile(),"The backupvars file is not a Poco file.");
 }
 
 void backupvars::createFromServiceLua(std::string imagename, const servicelua::luafile & syf)
 {
-   poco_assert(mVolumes.size() == 0);
-   poco_assert(mImageName.length() == 0);
+   drunner_assert(mVolumes.size() == 0, "Dirty volumes.");
+   drunner_assert(mImageName.length() == 0,"Dirty imagename.");
+   drunner_assert(imagename.length() > 0, "Empty imagename.");
    mImageName = imagename;
    syf.getBackupDockerVolumeNames(mVolumes);
 }
 
 cResult backupvars::loadvars()
 {
-   std::ofstream ifs(mPath.toString());
+   std::ifstream ifs(mPath.toString());
    if (ifs.bad())
       return kRError;
-   cereal::JSONOutputArchive archive(ifs);
+   cereal::JSONInputArchive archive(ifs);
    archive(*this);
+
+   drunner_assert(mImageName.length() > 0, "Empty imagename.");
+
    return kRSuccess;
 }
 
 cResult backupvars::savevars() const
 {
+   drunner_assert(mImageName.length() > 0, "Empty imagename.");
+
    std::ofstream os(mPath.toString());
    if (os.bad())
       return kRError;
