b58f4cd40111865514221b30509a268bd96a85da
j842 <j@842.be>
2016-08-02 18:22:55 +1200

    Fix all linker warnings..

---
+++ buildnum/build_number.h
@@ -0,0 +1,9 @@
+#ifndef BUILD_NUMBER_STR
+#define BUILD_NUMBER_STR "0.6 r113"
+#endif
+#ifndef VERSION_STR
+#define VERSION_STR "0.6 r113 - Mon Aug  1 21:05:15 NZST 2016"
+#endif
+#ifndef VERSION_STR_SHORT
+#define VERSION_STR_SHORT "0.6 r113"
+#endif
