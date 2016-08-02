89198aac56f32758b8a50233444f8e747d9b4369
j842 <j@842.be>
2016-08-02 20:57:25 +1200

    Full static (with no warnings).

--- unittest/drunnercfg.sh
+++
@@ -1,7 +0,0 @@
-# /home/j/dev/drunner/unittest/drunnercfg.sh
-# dRunner bash configuration file.
-ROOTPATH="/home/j/dev/drunner/unittest"
-ROOTUTILIMAGE="drunner/rootutils"
-DRUNNERINSTALLURL="https://drunner.s3.amazonaws.com/drunner-install"
-DRUNNERINSTALLTIME="Mon Jul  4 18:45:22 2016"
-PULLIMAGES=yes
