aebd18f489b90a340369b7042bc2b1c8b53bb986
j842 <j@842.be>
2016-08-02 22:42:44 +1200

    Attempt to get appveyor working

---
+++ pocowin/pocowin.ps1
@@ -0,0 +1,12 @@
+$POCO_URL ="http://pocoproject.org/releases/poco-1.7.3/poco-1.7.3.zip"
+
+wget $POCO_URL -Outfile poco.zip
+
+Expand-Archive "poco.zip" -DestinationPath ".\\"
+
+pushd poco-1.7.3
+.\\buildwin.cmd 140 build static_mt both Win32 nosamples notests
+popd
+
+Write-Host "Press any key to continue ..."
+$x = $host.UI.RawUI.ReadKey("NoEcho,IncludeKeyDown")
