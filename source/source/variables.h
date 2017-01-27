32d4fa156775e88111888fd62b7d974f87241117
j842 <j@842.be>
2017-01-28 09:35:24 +1300

    Working on variables.

--- source/source/variables.h
+++ source/source/variables.h
@@ -14,39 +14,10 @@
 
 typedef std::map<std::string, std::string> tKeyVals;
 
-// If configtypes are added then to_configtype needs to be updated.
-enum configtype
-{
-   kCF_port,
-   kCF_path,
-   kCF_existingpath,
-   kCF_string,
-   kCF_bool,
-   kCF_URL,
-   kCF_password,
-};
-
-configtype to_configtype(std::string s);
-
-class Configuration
-{
+class keyVals {
 public:
-   Configuration(std::string n, std::string dflt, std::string desc, configtype t, bool rqd, bool user) : name(n), defaultval(dflt), description(desc), type(t), required(rqd), usersettable(user) {}
-   
-   std::string name;
-   std::string defaultval;
-   std::string description;
-   configtype type;
-   bool required;
-   bool usersettable;
-};
-
-
-class variables {
-public:
-   variables() {}
-   //variables(const variables & other);
-   variables(const variables & other1, const variables & other2);
+   keyVals() {}
+   keyVals(const keyVals & other1, const keyVals & other2);
 
    bool hasKey(std::string key) const;
    bool isDefined(std::string key) const; // hasKey and is not empty string.
@@ -56,53 +27,75 @@ public:
    void delKey(std::string key);
 
    std::string substitute(std::string s) const;
-   const tKeyVals & getAll() const { return mVariables; }
+   const tKeyVals & getAll() const { return mKeyVals; }
 
 private:
-   tKeyVals mVariables;
+   tKeyVals mKeyVals;
 
    // --- serialisation --
    friend class cereal::access;
-   template <class Archive> void save(Archive &ar, std::uint32_t const version) const { ar(mVariables); }
-   template <class Archive> void load(Archive &ar, std::uint32_t const version) { mVariables.clear(); ar(mVariables); }
+   template <class Archive> void save(Archive &ar, std::uint32_t const version) const { ar(mKeyVals); }
+   template <class Archive> void load(Archive &ar, std::uint32_t const version) { mKeyVals.clear(); ar(mKeyVals); }
    // --- serialisation --
 };
-CEREAL_CLASS_VERSION(variables, 1);
+CEREAL_CLASS_VERSION(keyVals, 1);
 
 
-class persistvariables {
+
+const unsigned int ENV_DEFAULTS = 0x0000; // not user settable, memory only, not secret.
+const unsigned int ENV_USERSETTABLE = 0x0001;
+const unsigned int ENV_PERSISTS = 0x0002;
+const unsigned int ENV_SECRET = 0x0004;
+
+
+class envFlags {
 public:
-   persistvariables(std::string name, Poco::Path path, const std::vector<Configuration> config);
+   unsigned int state;
+};
+
+
+class envDef
+{
+public:
+   envDef(std::string n, std::string dflt, std::string desc, envFlags flags) : name(n), defaultval(dflt), description(desc)
+   {
+      usersettable = ((flags.state & ENV_USERSETTABLE) != 0);
+      persists = ((flags.state & ENV_PERSISTS) != 0);
+      secret = ((flags.state & ENV_SECRET) != 0);
+   }
+
+   std::string name;
+   std::string defaultval;
+   std::string description;
+   bool usersettable;
+   bool persists;
+   bool secret;
+};
+
+
+class persistvariables : public keyVals {
+public:
+   persistvariables(std::string name, Poco::Path path, const std::vector<envDef> config);
    cResult loadvariables();
    cResult savevariables() const;
-   cResult checkRequired() const;
-   cResult setVal(std::string key, std::string val);
-   void setVal_mem(std::string key, std::string val);
 
-   // expose other methods.
-   bool hasKey(std::string key) const;
-   bool isDefined(std::string key) const; // hasKey and is not empty string.
-   std::string getVal(std::string key) const;
-   bool getBool(std::string key) const;
-   std::string substitute(std::string s) const;
-   const tKeyVals getAll() const;
-   const variables getVariables() const;
+   const keyVals getVariables() const;
    bool exists() const;
+
+   // override base class, check it exists in the environment definitions before commiting.
+   cResult setVal(std::string key, std::string val);
    
    cResult handleConfigureCommand(CommandLine cl);
 
-
 protected:
    cResult _showconfiginfo() const;
-   cResult _checkvalid(std::string key, std::string val, Configuration config);
-   void _addConfig(const Configuration & c);
+   void _addConfig(const envDef & c);
+
+   std::unique_ptr<envDef> getDef(std::string key) const;
 
    std::string mName; // name of service (or whatever) to print nice messages.
    Poco::Path mPath;
-   std::vector<Configuration> mConfig;
-
-   variables mVariables;
-   variables mVariables_Mem;
+   std::vector<envDef> mEnvDefs;
 };
 
 #endif
